50151bd8f46a2c24bff539a58b36e5c407407202
Harshal0902 <harshalraikwar07@gmail.com>
2022-03-26 18:10:58 +0530

    ✨ Add Home page

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar';
 import Home from '../pages/Home';
+import Learn from '../pages/Learn';
+import Chatroom from '../pages/Chatroom'
 
 function Main() {
     return (
@@ -8,6 +10,8 @@ function Main() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/learn" element={<Learn />} />
+                <Route path="/chat" element={<Chatroom />} />
             </Routes>
         </BrowserRouter>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react"
+import HeroImg from '../assets/hero.svg'
+import InspirationImg from '../assets/feat.svg'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
+
   return (
-    <div>Home</div>
+    <div className='md:mx-28 mx-4 text-white pt-10 pb-12'>
+
+      <div className='md:grid md:grid-cols-2 items-center pt-10'>
+        <div className=''>
+          <h1 className='text-3xl md:text-6xl'>Who we are</h1>
+          <p className='text-xl md:text-2xl py-4 tracking-wider'></p>
+
+          <Link to="/learn">
+            <button className='bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl'>Learn</button>
+          </Link>
+
+        </div>
+        <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
+          <img src={HeroImg} alt="img" width="500" height="500" />
+        </div>
+      </div>
+
+      <div className='md:grid md:grid-cols-2 pt-12 items-center'>
+        <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
+          <img src={InspirationImg} alt="img" width="500" height="500" />
+        </div>
+        <div className=''>
+          <h1 className='text-3xl md:text-6xl'>What else do we have</h1>
+          <p className='text-xl md:text-2xl py-4 tracking-wider'> LearnEasy is a web app where you can:
+          </p>
+          <ul className="text-xl">
+            <li className="list-disc">Features</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
